b85afe191028d742504662e114e23a722c91508d
szilard <szilard@users.noreply.github.com>
2016-02-10 20:50:07 -0800

    updates

--- plot.R
+++ plot.R
@@ -3,7 +3,7 @@ library(ggplot2)
 
 ticks <- c(0.3,1,3,10,30,100,300)
 
-d <- read.csv("plot.csv", sep="|") 
+d <- read.csv("plot.csv", sep="|", comment.char = "#") 
 names(d) <- tolower(names(d))
 
 d %>% ggplot(aes(x = aggregation, y = join, label = system, color = type)) +
