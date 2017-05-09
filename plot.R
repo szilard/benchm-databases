9e4d0560b08fcf3c9fdab7338258463f077cb285
szilard <szilard@users.noreply.github.com>
2017-05-09 16:34:50 -0700

    mapd

--- plot.R
+++ plot.R
@@ -1,15 +1,15 @@
 library(dplyr)
 library(ggplot2)
 
-ticks <- c(0.3,1,3,10,30,100,300)
+ticks <- c(0.1,0.3,1,3,10,30,100,300)
 
 d <- read.csv("plot.csv", sep="|", comment.char = "#") 
 names(d) <- tolower(names(d))
 
 d %>% ggplot(aes(x = aggregation, y = join, label = system, color = type)) +
     geom_text() +
-    scale_x_log10(breaks = ticks, labels = ticks, limits = c(0.3,500)) + 
-    scale_y_log10(breaks = ticks, labels = ticks, limits = c(0.3,500)) +
+    scale_x_log10(breaks = ticks, labels = ticks, limits = c(0.1,500)) + 
+    scale_y_log10(breaks = ticks, labels = ticks, limits = c(0.1,500)) +
     geom_smooth(aes(x = aggregation, y = join), method = "lm", se = FALSE,
                 formula = y ~ x + 0, color="grey70")
 
